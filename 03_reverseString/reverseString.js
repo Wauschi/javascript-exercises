9beaacc1cd929e15c90640aa64b450455657f45e
Michael Schaal <miguel-ms@hotmail.de>
2023-06-04 02:42:03 +0200

    Added function that splits the string into letters, reverses them with a loop and puts them back together

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let ArrayOfLetters = string.split("")
+    let output = ""
+    
+    for (let index = ArrayOfLetters.length-1; index >= 0; index--) {
+        output = output + ArrayOfLetters[index];
+    }
+    return output;
 };
 
 // Do not edit below this line
