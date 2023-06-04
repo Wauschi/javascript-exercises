c37fcd067b7a2ea818df458bafd145f99cbde0ae
Michael Schaal <miguel-ms@hotmail.de>
2023-06-04 02:12:00 +0200

    Implement function with output var and concat of string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+    var output = "";
+    for (let index = 0; index < number; index++) {
+        output = output + string;
+        
+    }
+    return output;
 };
 
 // Do not edit below this line
