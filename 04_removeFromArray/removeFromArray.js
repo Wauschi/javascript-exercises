f7d45d239286c6301d630dae5fc0a46f0dc26c3f
Michael Schaal <miguel-ms@hotmail.de>
2023-06-04 03:08:24 +0200

    added function to get the index of the items that are to be removed and remove them from the array with splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(Array,...RemoveObject) {
+    
+    for (let index = 0; index < RemoveObject.length; index++) {
 
+        let stelle = Array.indexOf(RemoveObject[index])
+        if (stelle >-1) {
+            Array.splice(stelle,1)
+        }
+        
+        
+    }
+    return Array
 };
 
 // Do not edit below this line
