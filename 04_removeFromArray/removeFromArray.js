1f525d68befc9868b8f9ceeac59cb6724ae66544
Michael Schaal <miguel-ms@hotmail.de>
2023-06-09 22:39:06 +0200

    added sumalljs in which the numbers are added up between the two numbers, while also checking the numbers are nu mbers

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,9 @@
 const removeFromArray = function(Array,...RemoveObject) {
-    
     for (let index = 0; index < RemoveObject.length; index++) {
-
         let stelle = Array.indexOf(RemoveObject[index])
         if (stelle >-1) {
             Array.splice(stelle,1)
         }
-        
-        
     }
     return Array
 };
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    let sum = 0
+    let nr1 = 0;
+    let nr2 = 0;
+    if(typeof number1 !== 'number'){
+        return("ERROR")
+    }
+    if(typeof number2 !== 'number'){
+        return("ERROR")
+    }
+    if(number1 <0){
+        return("ERROR")
+    }
+    if(number2 <0){
+        return("ERROR")
+    }
+    if (number1 > number2 ) { 
+        nr1 = number2;
+        nr2 = number1;
+    } else {
+        nr1 = number1;
+        nr2 = number2;
+    }
+    for (let index = nr1; index <= nr2; index++) {
+        sum = sum + index;
+        
+    }
+    return sum
 };
 
 // Do not edit below this line
