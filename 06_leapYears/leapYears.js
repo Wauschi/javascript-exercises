5b21b7e8e724fafd3785cd1fc51651989049b2b6
Michael Schaal <miguel-ms@hotmail.de>
2023-06-09 22:54:19 +0200

    added function to check if something is a leapyear according to the rules, also the test things

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (nr) {
+    if(nr % 4 == 0){
+        if(nr % 100 == 0){
+            if(nr % 400 == 0){
+                return(true)
+            }
+            return(false)
+        }
+        return(true)
+    } else{
+        return(false)
+    }
 };
 
 // Do not edit below this line
