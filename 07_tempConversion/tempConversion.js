508d9843ff9cb91bf71a065bbd82d3f763b030a4
Michael Schaal <miguel-ms@hotmail.de>
2023-06-09 23:11:58 +0200

    added function to convert fahrenheit to celsius *using the rules but also checking if the converted number would have a decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempconverted = (temp - 32) * (5/9) 
+  if (tempconverted % 1 == 0) {
+    return tempconverted
+  } else {
+    return(Number(tempconverted.toFixed(1)));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempconverted = temp *(9/5) +32;
+  if(tempconverted % 1 == 0){
+    return tempconverted
+  } else {
+    return(Number(tempconverted.toFixed(1)));
+  }
+  
 };
 
 // Do not edit below this line
